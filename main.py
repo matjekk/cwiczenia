69734abf6daf420d05f79e6317bf200eb62e7111
mckaeye <>
2022-01-30 16:48:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+
+your_car = 'Skoda' 
+
+
+german_cars = ['Mercedes', 'Audi', 'Volkswagen', 'Opel', 'BMW', 'Porsche', ]
+
+french_cars = ['Citroen', 'Peugeot', 'Renault', 'Dacia']
+
+japanese_cars = ['Toyota', 'Infiniti', 'Mazda', 'Lexus', 'Mitsubishi', 'Nissan', 'Suzuki', 'Subaru', 'Honda']
+
+italian_cars = ['Alfa romeo', 'Fiat', 'Lancia', 'Lamborghini']
+
+spanish_cars = ['Seat']
+
+czech_cars = ['Skoda']
+
+american_cars = ['Cadillac', 'Chevrolet', 'Ford', 'Chrysler', 'Dodge', 'Jeep', 'Tesla', 'Pontiac']
+
+swedish_cars = ['Volvo']
+
+british_cars = ['Jaguar', 'Mini', 'Land Rover', 'Bentley']
+
+southern_Korean_cars = ['Daewoo', 'Kia']
+
+polish_cars = ['Izera']
+
+
+
+
+if your_car in german_cars:
+  print('Your car is German!')
+
+elif your_car in french_cars:
+  print('Your car is French!')
+
+elif your_car in polish_cars:
+  print('Your car is polish XD')
+
+elif your_car in japanese_cars:
+  print('Your car is Japanese!')
+
+elif your_car in italian_cars:
+  print('Your car is Italian!')
+
+elif your_car in spanish_cars:
+  print('Your car is Spanish!')
+
+elif your_car in czech_cars:
+  print('Your car is Czech!')
+
+elif your_car in american_cars:
+  print('Your car is American!')
+
+elif your_car in swedish_cars:
+  print('Your car is Swedish!')
+
+elif your_car in british_cars:
+  print('Your car is British!')
+
+elif your_car in southern_Korean_cars:
+  print('Your car is South Korean!')
+
+else:
+  print('Oops! Looks like something is wrong. Try using capital letter as the first letter')
